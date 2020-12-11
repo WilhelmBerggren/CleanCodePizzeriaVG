0c31dd7060784945eba57adeeb94ecedb5bfb674
Nils Lundell <Nils.Lundell@mardigras.se>
2020-12-11 13:26:46 +0100

    Added more spaghetti to refactor!

--- CleanCodePizzeria/CleanCodePizzeria/Program.cs
+++ CleanCodePizzeria/CleanCodePizzeria/Program.cs
@@ -8,18 +8,58 @@ namespace CleanCodePizzeria
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-
-            var pizzeria = Pizzeria.GetPizzeria();
-            var order = pizzeria.StartOrder();
             var menuVisitor = new PizzeriaVisitor();
+            var pizzeria = Pizzeria.GetPizzeria();
+            var orderManager = new OrderManager(pizzeria);
+
+            Console.WriteLine("Hello!\n[0]Costumer\n[1]Employee");
+            var input = Console.ReadLine();
+            switch (input)
+            {
+                case "0":
+                    var order = pizzeria.StartOrder();
+                    Console.WriteLine("Add an item:");
+                    while (true)
+                    {
+                        orderManager.Start();
+                    }
+                break;
+
+                case "1":
+                    var accumulator = 0;
+                    foreach (var item in pizzeria.OrdersInProgress)
+                    {
+                        Console.WriteLine($"[{accumulator}]{menuVisitor.VisitOrder(item)}");
+                        accumulator++;
+                    }
+                    Console.WriteLine(Environment.NewLine + "Choose an order");
+                    var orderIndex = int.Parse(Console.ReadLine());
+                    var currentOrder = pizzeria.OrdersInProgress.ToArray()[orderIndex];
+                    Console.WriteLine("[0]Complete order\n[1]Remove order");
+                    input = Console.ReadLine();
+                    if (input == "0")
+                    {
+                        pizzeria.CompleteOrder(currentOrder);
+                        Console.WriteLine("order completed");
+                    }
+                    if (input == "1")
+                    { 
+                        pizzeria.Orders.Remove(currentOrder.ID);
+                        Console.WriteLine("order removed");
+                    } 
+                break;
+            }
+
+
+
+            
 
             Console.WriteLine("updated order");
             var pizza = pizzeria.Pizzas[0];
             pizza.Ingredients.Add(pizzeria.Extras[0]);
-            order.AddItem(pizza);
-            pizzeria.UpdateOrder(order);
-            pizzeria.CompleteOrder(order);
+            //order.AddItem(pizza);
+            //pizzeria.UpdateOrder(order);
+            //pizzeria.CompleteOrder(order);
 
             Console.WriteLine("completed order");
             Console.WriteLine("Not completed: ");
