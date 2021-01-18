0bcf6fc00a0a6cecef1c6c68910d76ed5a082b73
Wilhelm Berggren <wilhelmberggren@hotmail.com>
2021-01-18 10:47:08 +0100

    Add stock controller

---
+++ CleanCodePizzeria/PizzeriaStock/Controllers/StockController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using CleanCodePizzeria;
+using CleanCodePizzeria.Models;
+
+namespace PizzeriaStock.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class StockController : ControllerBase
+    {
+        private static PizzaRepository repository = new PizzaRepository();
+
+        private readonly ILogger<StockController> _logger;
+
+        public StockController(ILogger<StockController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<Pizza> Get()
+        {
+            return repository.GetPizzas();
+        }
+
+        [HttpGet("Extras")]
+        public IEnumerable<Ingredient> GetExtras() {
+            return repository.GetExtras();
+        }
+
+        [HttpGet("Drinks")]
+        public IEnumerable<Drink> GetDrinks() {
+            return repository.GetDrinks();
+        }
+    }
+}
